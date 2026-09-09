a1d701debeea3c53cd7fcd7f95e65213c231ff43
unknown <congchien1307@gmail.com>
2026-09-10 01:39:02 +0700

    read HX711, calculate data

--- Driver/HX711.c
+++ Driver/HX711.c
@@ -1,12 +1,15 @@
 #include "HX711.h"
 #include "STM32_reg.h"
-#include "stdint.h"
+#include "GPIO.h"
+#include "SysTick.h"
 #define GAIN_128 25
 #define GAIN_64 27
 #define GAIN_32 26
+// check if HX711 is ready for data retrieval
 int8_t HX711_check(void){
-    return (HX711_DT_read()==0);
+    return (HX711_DT_Read()==0);
 }
+// read 24-bit data from HX711
 int32_t HX711_read_data(uint8_t gain)
 {
     int32_t data = 0;
@@ -24,9 +27,38 @@ int32_t HX711_read_data(uint8_t gain)
         HX711_SCK_LOW();
         Delay_us(1);
     }
-    if (data & 0x800000){
+    if (data & 0x800000){        // if the sign bit is set, extend the sign to 32 bits
         data |= 0xFF000000; 
     }
     return (uint32_t)data;
 }
 
+// calculate the offset value by averaging multiple samples
+uint32_t HX711_tare(uint16_t samples){
+    uint64_t sum = 0;
+    for (int i = 0; i < samples; i++){
+        sum += HX711_read_data(GAIN_128);
+    }
+    return (uint32_t)(sum / samples);
+}
+
+// calibrate the scale with known weight
+void HX711_calibrate(HX711_Cal_t *cal, uint16_t samples, float known_weight){
+    uint64_t sum = 0;
+    cal->offset = HX711_tare(samples);
+    for (int i = 0; i < samples; i++){
+        sum += HX711_read_data(GAIN_128);
+    }
+    uint32_t avg = (uint32_t)(sum / samples);
+    cal->scale = (float)(avg - cal->offset) / known_weight;
+}
+
+// get weight in grams
+float HX711_get_weight(HX711_Cal_t *cal, uint16_t samples){
+    uint64_t sum = 0;
+    for (int i = 0; i < samples; i++){
+        sum += HX711_read_data(GAIN_128);
+    }
+    uint32_t avg = (uint32_t)(sum / samples);
+    return (float)(avg - cal->offset) / cal->scale;
+}
