58885a435c049c951cf49eb15a4df1ff95367f10
unknown <congchien1307@gmail.com>
2026-09-10 17:00:50 +0700

    configure HX711

--- Driver/HX711.h
+++ Driver/HX711.h
@@ -1,6 +1,7 @@
 #ifndef HX711_H
 #define HX711_H
-
+#include <stdint.h>
+#include "STM32_reg.h"
 typedef struct {
     int32_t offset;
     float   scale;
@@ -8,4 +9,9 @@ typedef struct {
 
 int8_t HX711_check(void);
 int32_t HX711_read_data(uint8_t gain);
+uint32_t HX711_tare(uint16_t samples);
+void HX711_calibrate(HX711_Cal_t *cal, uint16_t samples, float known_weight);
+float HX711_get_weight(HX711_Cal_t *cal, uint16_t samples);
+void HX711_savetoflash(HX711_Cal_t *cal);
+void HX711_Loadfromflash(HX711_Cal_t *cal);
 #endif
